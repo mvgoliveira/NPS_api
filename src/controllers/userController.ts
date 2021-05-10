2a932b1b61c9482f187dd5fc4dd73d6b8068af31
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2021-05-09 21:48:00 -0300

    ✨ Added surveys functions to database control

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,12 @@
-import { request, Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UserRepository';
 
 class UserController {
    async create( req: Request, res: Response ) {
       const { name, email } = req.body;
       
-      const usersRepository = getRepository(User);
+      const usersRepository = getCustomRepository(UsersRepository);
 
       const userAlreadyExists = await usersRepository.findOne({
          email
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import { SurveyController } from './controllers/surveyController';
 import { UserController } from './controllers/userController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveyController();
 
 router.post("/users", userController.create);
+
+router.post("/surveys", surveyController.create);
+router.get("/surveys", surveyController.list);
+
 
 export { router };
