bdbc82dcc8dc2406e4e23f59d6339ca8766af9d5
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2021-05-12 14:45:26 -0300

    🩹 Fix id fom query params in link to answer

--- src/controllers/sendMailController.ts
+++ src/controllers/sendMailController.ts
@@ -27,27 +27,26 @@ class SendMailController {
          res.status(400).json({ error: "survey does not exists" })
       }
 
-      const variables = {
-         name: user.name, 
-         title: survey.title, 
-         description: survey.description,
-         user_id: user.id,
-         link: process.env.URL_MAIL
-      }
-
-      const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
-
       const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
          where: {user_id: user.id, value: null},
          relations: ["user", "survey"]
       })
 
+      const variables = {
+         name: user.name, 
+         title: survey.title, 
+         description: survey.description,
+         id: "",
+         link: process.env.URL_MAIL
+      }
+
+      const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
       if (surveyUserAlreadyExists) {
+         variables.id = surveyUserAlreadyExists.id;
          await SendMailService.send(email, survey.title, variables, npsPath);
          return res.json(surveyUserAlreadyExists);
       }
-      
-      await SendMailService.send(email, survey.title, variables, npsPath);
       
       //salvar informações na tabela
       const surveyUser = surveysUsersRepository.create({
@@ -56,6 +55,12 @@ class SendMailController {
       })
       
       await surveysUsersRepository.save(surveyUser);
+
+      variables.id = surveyUser.id;
+
+      await SendMailService.send(email, survey.title, variables, npsPath);
+      
+      return res.json(surveyUser);
    }
 }
 
